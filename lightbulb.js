d6595e90246f59dc29ea421b3b5582d764864d53
Glen Larkin <glenlarkin@gmail.com>
2020-10-29 17:47:09 -0600

    change photos to lando?

--- lightbulb.js
+++ lightbulb.js
@@ -1,7 +1,4 @@
-function lightBulb() {
+function lightOn() {
     
-    var onOff = document.getElementById("btn1").value;
-    if
-    document.getElementById('myImage').src='pic_bulbon.gif';
-    document.getElementById('myImage').src='pic_bulboff.gif';
-  }
+        document.getElementById('photo').src="lando_hat.jpeg";
+}
