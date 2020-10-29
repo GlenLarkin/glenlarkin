517e39e5afc5df176680e4458eebde5f702a9492
Glen Larkin <glenlarkin@gmail.com>
2020-10-29 17:32:54 -0600

    separate page for javascript implementation

---
+++ lightbulb.js
@@ -0,0 +1,7 @@
+function lightBulb() {
+    
+    var onOff = document.getElementById("btn1").value;
+    if
+    document.getElementById('myImage').src='pic_bulbon.gif';
+    document.getElementById('myImage').src='pic_bulboff.gif';
+  }
