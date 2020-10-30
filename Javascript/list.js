3a24b0480070a37420a834520960c7c5fddbea29
Glen Larkin <glenlarkin@gmail.com>
2020-10-30 17:11:16 -0600

    Future implement this to create responsive display sizing

---
+++ Javascript/list.js
@@ -0,0 +1,7 @@
+// An array with some elements
+var fruits = ["Apple", "Banana", "Mango", "Orange", "Papaya"];
+     
+// Loop through all the elements in the array 
+for(var i in fruits) {  
+    document.write("<p>" + fruits[i] + "</p>");
+}
---
+++ Javascript/windowDimensions.js
@@ -0,0 +1,5 @@
+function windowSize() {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    alert("Width: " + w + ", " + "Height: " + h);
+}
